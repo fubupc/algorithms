7897bb103227b8354d786b2665103a52c8374c86
fubupc <fubupc@gmail.com>
2016-01-20 18:41:59 +0800

    Breadth first search (multiple sources)

--- ch4/4.1/BreadthFirstSearch.java
+++ ch4/4.1/BreadthFirstSearch.java
@@ -6,16 +6,44 @@ class BreadthFirstSearch {
     private int[] edgeTo;
     private int[] distTo;
 
-    private final int s;
-
     public BreadthFirstSearch(Graph g, int s) {
         marked = new boolean[g.V()];
         edgeTo = new int[g.V()];
         distTo = new int[g.V()];
 
-        this.s = s;
-
         bfs(g, s);
+    }
+
+    public BreadthFirstSearch(Graph G, int[] sources) {
+        marked = new boolean[G.V()];
+        edgeTo = new int[G.V()];
+        distTo = new int[G.V()];
+
+        bfs(G, sources);
+    }
+
+    public void bfs(Graph G, int[] sources) {
+        Queue<Integer> q = new LinkedList<Integer>();
+
+        for (int s : sources) {
+            marked[s] = true;
+            edgeTo[s] = s;
+            distTo[s] = 0;
+            q.add(s);
+        }
+
+        while (!q.isEmpty()) {
+            int v = q.remove();
+
+            for (int w : G.adj(v)) {
+                if (!marked[w]) {
+                    marked[w] = true;
+                    edgeTo[w] = v;
+                    distTo[w] = distTo[v] + 1;
+                    q.add(w);
+                }
+            }
+        }
     }
 
     public void bfs(Graph g, int s) {
@@ -31,7 +59,6 @@ class BreadthFirstSearch {
 
             for (int w : g.adj(v)) {
                 if (!marked[w]) {
-                    System.out.format("enqueu: %d\n", w);
                     marked[w] = true;
                     edgeTo[w] = v;
                     distTo[w] = distTo[v] + 1;
@@ -50,11 +77,12 @@ class BreadthFirstSearch {
 
         Stack<Integer> path = new Stack<Integer>();
 
-        for (int w = v; w != this.s; w = edgeTo[w]) {
+        int w = v;
+        for (; w != edgeTo[w]; w = edgeTo[w]) {
             path.push(w);
         }
 
-        path.push(this.s);
+        path.push(w);
 
         return path;
     }
@@ -64,17 +92,37 @@ class BreadthFirstSearch {
     }
 
     public static void main(String[] args) {
+        BreadthFirstSearch bfs;
+
         Graph g = new Graph(new In("tinyG.txt"));
         System.out.println(g);
 
         int s = 3;
-        BreadthFirstSearch paths = new BreadthFirstSearch(g, s);
+        bfs = new BreadthFirstSearch(g, s);
 
+        System.out.println("Single Sources 3:");
         for (int v = 0; v < g.V(); v++) {
-            if (paths.hasPathTo(v)) {
-                System.out.format("%d to %d (%d): ", s, v, paths.distTo(v));
+            if (bfs.hasPathTo(v)) {
+                System.out.format("%d to %d (%d): ", s, v, bfs.distTo(v));
 
-                for (int w : paths.pathTo(v)) {
+                for (int w : bfs.pathTo(v)) {
+                    System.out.format(w + " ");
+                }
+            } else {
+                System.out.format("%d to %d: not connected.", s, v);
+            }
+            System.out.format("\n");
+        }
+
+        int[] sources = {3, 6, 12};
+        bfs = new BreadthFirstSearch(g, sources);
+
+        System.out.println("\nMultiple Sources (3, 6, 12):");
+        for (int v = 0; v < g.V(); v++) {
+            if (bfs.hasPathTo(v)) {
+                System.out.format("%d (%d): ", v, bfs.distTo(v));
+
+                for (int w : bfs.pathTo(v)) {
                     System.out.format(w + " ");
                 }
             } else {
