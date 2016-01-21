4d9198208510e35723c25ced29137d79e5f8164b
fubupc <fubupc@gmail.com>
2016-01-21 23:07:25 +0800

    Replace java.util.Stack with Stack from book

--- ch4/4.1/BreadthFirstPaths.java
+++ ch4/4.1/BreadthFirstPaths.java
@@ -1,5 +1,4 @@
 import java.util.Queue;
-import java.util.Stack;
 import java.util.LinkedList;
 
 class BreadthFirstPaths {
--- ch4/4.1/Graph.java
+++ ch4/4.1/Graph.java
@@ -85,6 +85,12 @@ class Graph {
         return out;
     }
 
+    public static void main(String[] args) {
+        Graph g = new Graph(new In("tinyG.txt"));
+        System.out.println(g);
+        System.out.println(new Graph(g));
+    }
+
 }
 
 
