3e0d4feca7af3cfd55182150144a26e46343b046
fubupc <fubupc@gmail.com>
2016-01-25 19:57:05 +0800

    Maze: simplify with coordinate re-interpretation

--- ch4/4.1/Maze.java
+++ ch4/4.1/Maze.java
@@ -1,41 +1,44 @@
 /* Perfect maze generator
- */
+*/
 import java.util.ArrayList;
 import java.util.List;
 
 public class Maze {
-    private int rows;
-    private int cols;
+    private int N;
 
     private boolean[][] marked;
-    private boolean[][] east;
-    private boolean[][] south;
 
-    public static final int E = 1;
+    private boolean[][] north;
+    private boolean[][] south;
+    private boolean[][] west;
+    private boolean[][] east;
+
+    public static final int N2 = 1;
     public static final int S = 2;
     public static final int W = 3;
-    public static final int N = 4;
+    public static final int E = 4;
 
-    public Maze(int r, int c) {
-        rows = r + 2;
-        cols = c + 2;
+    public Maze(int N) {
+        this.N = N;
 
-        marked = new boolean[rows][cols];
-        east = new boolean[rows][cols];
-        south = new boolean[rows][cols];
+        marked = new boolean[N + 2][N + 2];
 
-        for (int i = 0; i < rows; i++) {
-            marked[i][0] = true;
-            marked[i][cols - 1] = true;
-            south[i][0] = true;
-            south[i][cols - 1] = true;
-        }
+        north = new boolean[N + 2][N + 2];
+        south = new boolean[N + 2][N + 2];
+        west = new boolean[N + 2][N + 2];
+        east = new boolean[N + 2][N + 2];
 
-        for (int j = 0; j < cols; j++) {
-            marked[0][j] = true;
-            marked[rows - 1][j] = true;
-            east[0][j] = true;
-            east[rows - 1][j] = true;
+        for (int x = 0; x < N + 2; x++) {
+            for (int y = 0; y < N + 2; y++) {
+                if (x == 0 || x == N + 1 || y == 0 || y == N + 1) {
+                    marked[x][y] = true;
+                } else {
+                    north[x][y] = true;
+                    south[x][y] = true;
+                    west[x][y] = true;
+                    east[x][y] = true;
+                }
+            }
         }
 
         dfs(1, 1);
@@ -52,40 +55,37 @@ public class Maze {
             int rand = (int) (Math.random() * l.size());
             int next = l.get(rand);
 
-            if (next == E) {
-                east[x][y] = true;
+            if (next == N2) {
+                north[x][y] = false;
+                south[x][y + 1] = false;
+
                 dfs(x, y + 1);
             } else if (next == S) {
-                south[x][y] = true;
-                dfs(x + 1, y);
-            } else if (next == W) {
-                east[x][y - 1] = true;
+                south[x][y] = false;
+                north[x][y - 1] = false;
+
                 dfs(x, y - 1);
-            } else {
-                south[x - 1][y] = true;
+            } else if (next == W) {
+                west[x][y] = false;
+                east[x - 1][y] = false;
+
                 dfs(x - 1, y);
-            }
+            } else if (next == E) {
+                east[x][y] = false;
+                west[x + 1][y] = false;
+
+                dfs(x + 1, y);
+            } 
         }
     }
 
     public List<Integer> adj(int x, int y) {
         List<Integer> l = new ArrayList<Integer>();
 
-        if (!marked[x][y + 1]) {
-            l.add(E);
-        }
-
-        if (!marked[x + 1][y]) {
-            l.add(S);
-        }
-
-        if (!marked[x][y - 1]) {
-            l.add(W);
-        }
-
-        if (!marked[x - 1][y]) {
-            l.add(N);
-        }
+        if (!marked[x][y + 1]) l.add(N2);
+        if (!marked[x][y - 1]) l.add(S);
+        if (!marked[x + 1][y]) l.add(E);
+        if (!marked[x - 1][y]) l.add(W);
 
         return l;
     }
@@ -95,14 +95,14 @@ public class Maze {
         String out = "";
         String chars = " ╴╷┐╶─┌┬╵┘│┤└┴├┼";
 
-        int t, r, b, l;  // if top, right, bottom, left has bar.
-       
-        for (int row = 1; row < rows; row++) {
-            for (int col = 1; col < cols; col++) {
-                t = toInt(!east[row - 1][col - 1]);
-                r = toInt(!south[row - 1][col]);
-                b = toInt(!east[row][col - 1]);
-                l = toInt(!south[row - 1][col - 1]);
+        int t = 0, r = 0, b = 0, l = 0;  // if top, right, bottom, left has bar.
+
+        for (int y = N + 1; y >= 1; y--) {
+            for (int x = 1; x <= N + 1; x++) {
+                if (west[x][y] || east[x - 1][y]) t = 1; else t = 0;
+                if (south[x][y] || north[x][y - 1]) r = 1; else r = 0;
+                if (west[x][y - 1] || east[x - 1][y - 1]) b = 1; else b = 0;
+                if (south[x - 1][y] || north[x - 1][y - 1]) l = 1; else l = 0;
 
                 int index = (t << 3) + (r << 2) + (b << 1) + l;
                 out += chars.charAt(index);
@@ -114,15 +114,10 @@ public class Maze {
         return out;
     }
 
-    public int toInt(boolean b) {
-        if (b == true) return 1;
-        else return 0;
-    }
-
     public void walk(int startX, int startY, int endX, int endY) {
-        for (int i = 1; i < rows - 1; i++) {
-            for (int j = 1; j < cols - 1; j++) {
-                marked[rows][cols] = false;
+        for (int i = 1; i < N - 1; i++) {
+            for (int j = 1; j < N - 1; j++) {
+                marked[N][N] = false;
             }
         }
 
@@ -152,33 +147,26 @@ public class Maze {
     }
 
     public void draw() {
-        StdDraw.setScale(-0.05, 1.05);
-        StdDraw.setPenRadius(0.001);
+        StdDraw.setXscale(0, N + 2);
+        StdDraw.setYscale(0, N + 2);
         StdDraw.setPenColor(StdDraw.BLUE);
 
-        double ru = 1.0 / (rows - 2);
-        double cu = 1.0 / (cols - 2);
-
-        for (int i = 1; i < rows - 1; i++) {
-            for (int j = 1; j < cols - 1; j++) {
-                if (!east[i][j - 1]) {
-                    StdDraw.line((j - 1) * cu, (rows - i - 1) * ru, (j - 1) * cu, (rows - i - 2) * ru);
-                }
-
-                if (!south[i - 1][j]) {
-                    StdDraw.line((j - 1) * cu, (rows - i - 1) * ru, j * cu, (rows - i - 1) * ru);
-                }
+        for (int x = 1; x <= N; x++) {
+            for (int y = 1; y <= N; y++) {
+                if (north[x][y]) StdDraw.line(x, y + 1, x + 1, y + 1);
+                if (south[x][y]) StdDraw.line(x, y, x + 1, y);
+                if (west[x][y]) StdDraw.line(x, y, x, y + 1);
+                if (east[x][y]) StdDraw.line(x + 1, y, x + 1, y + 1);
             }
         }
 
-        StdDraw.line(0, 0, 1, 0);
-        StdDraw.line(1, 0, 1, 1);
     }
 
     public static void main(String[] args) {
-        Maze m = new Maze(20, 20);
+        Maze m = new Maze(20);
         System.out.println(m);
         m.draw();
+
     }
 
 }
