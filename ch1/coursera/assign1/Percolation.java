1c4b60bd415e9ab1badfc595f79f77dc5eb87027
fubupc <fubupc@gmail.com>
2016-02-06 11:09:38 +0800

    Percolation: fix the backwash problem!

--- ch1/coursera/assign1/Percolation.java
+++ ch1/coursera/assign1/Percolation.java
@@ -1,14 +1,20 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 import edu.princeton.cs.algs4.StdRandom;
 
+/* Resolve backwash problem 
+ *
+ * Reference: http://www.sigmainfy.com/blog/avoid-backwash-in-percolation.html
+ *
+ * */
+
 public class Percolation {
     /* Percolation grid size N. */
     private int N;
 
-    /* record open cells.
-     * NOTE: use one-dimensional to represent two-dimensional/table here.
-     */
-    private boolean[] opened;
+    /* record cell status: open, top connected, bottom connected.  */
+    private byte[] status;
+
+    private boolean percolates;
 
     /* WeightedQuickUnionUF */
     private WeightedQuickUnionUF uf;
@@ -18,17 +24,13 @@ public class Percolation {
             throw new IllegalArgumentException("N must be integer greater than 0.");
 
         this.N = N;
-        int total = (N + 2) * (N + 2);
+        percolates = false;
+        status = new byte[(N + 2) * (N + 2)];
+        uf = new WeightedQuickUnionUF((N + 2) * (N + 2));
 
-        opened = new boolean[total];
-        uf = new WeightedQuickUnionUF(total);
-
-        for (int i = 0; i < N + 2; i++) {
-            opened[i] = true;
-            opened[xyTo1D(N + 1, i)] = true;
-
-            uf.union(0, i);
-            uf.union(total - 1, xyTo1D(N + 1, i));
+        for (int i = 1; i <= N; i++) {
+            setTopConnected(xyTo1D(1, i));
+            setBottomConnected(xyTo1D(N, i));
         }
     }
 
@@ -43,32 +45,83 @@ public class Percolation {
                     y + ") is outside of range: " + N);
     }
 
-    public boolean isFull(int i, int j) {
-        checkXY(i, j);
-        return uf.connected(0, xyTo1D(i, j));
+    private void setTopConnected(int idx) {
+        status[idx] = (byte) (status[idx] | 0b100);
+    }
+
+    private void setBottomConnected(int idx) {
+        status[idx] = (byte) (status[idx] | 0b010);
+    }
+
+    private void setOpened(int idx) {
+        status[idx] = (byte) (status[idx] | 0b001);
+    }
+
+    private boolean topConnected(int idx) {
+        return (status[idx] & 0b100) >> 2 == 1;
+    }
+
+    private boolean bottomConnected(int idx) {
+        return (status[idx] & 0b010) >> 1 == 1;
+    }
+
+    private boolean isOpen(int idx) {
+        return (status[idx] & 0b001) == 1;
     }
 
     public boolean isOpen(int i, int j) {
         checkXY(i, j);
-        return opened[xyTo1D(i, j)];
+        return isOpen(xyTo1D(i, j));
+    }
+
+    public boolean isFull(int i, int j) {
+        checkXY(i, j);
+
+        int root = uf.find(xyTo1D(i, j));
+
+        return isOpen(xyTo1D(i, j)) && topConnected(root);
     }
 
     public boolean percolates() {
-        return uf.connected(0, (N + 2) * (N + 2) - 1);
+        return percolates;
     }
 
     public void open(int i, int j) {
         checkXY(i, j);
+
         int idx = xyTo1D(i, j);
 
-        if (opened[idx]) return;
+        if (isOpen(idx)) return;
 
-        opened[idx] = true;
+        setOpened(idx);
 
-        if (opened[xyTo1D(i - 1, j)]) uf.union(idx, xyTo1D(i - 1, j));
-        if (opened[xyTo1D(i + 1, j)]) uf.union(idx, xyTo1D(i + 1, j));
-        if (opened[xyTo1D(i, j - 1)]) uf.union(idx, xyTo1D(i, j - 1));
-        if (opened[xyTo1D(i, j + 1)]) uf.union(idx, xyTo1D(i, j + 1));
+        boolean top = false;
+        boolean bottom = false;
+
+        int[] neighbors = new int[4];
+        neighbors[0] = xyTo1D(i - 1, j);
+        neighbors[1] = xyTo1D(i + 1, j);
+        neighbors[2] = xyTo1D(i, j - 1);
+        neighbors[3] = xyTo1D(i, j + 1);
+
+        for (int n = 0; n < neighbors.length; n++) {
+            if (!isOpen(neighbors[n])) continue;
+
+            int root = uf.find(neighbors[n]);
+
+            if (topConnected(root)) top = true;
+            if (bottomConnected(root)) bottom = true;
+
+            uf.union(idx, root);
+        }
+
+        int newRoot = uf.find(idx);
+
+        if (top || topConnected(idx)) setTopConnected(newRoot);
+        if (bottom || bottomConnected(idx)) setBottomConnected(newRoot);
+
+        if (topConnected(newRoot) && bottomConnected(newRoot))
+            percolates = true;
     }
 
     /*
