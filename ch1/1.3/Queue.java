03efdd63df96af35352ddf55c7f645a85d6e2566
fubupc <fubupc@gmail.com>
2016-02-17 18:07:11 +0800

    Queue (Array-based) bugfix: index wrap-around

--- ch1/1.3/Queue.java
+++ ch1/1.3/Queue.java
@@ -8,60 +8,52 @@ public class Queue<Item> implements Iterable<Item> {
     private Item[] items;
     private int head;
     private int tail;
+    private int size;
 
     public Queue() {
         items = (Item[]) new Object[INIT_CAPACITY];
         head = -1;
         tail = -1;
+        size = 0;
     }
 
     public int size() {
-        if (head == -1) return 0;
-        else if (head <= tail) return tail - head + 1;
-        else return items.length - head + tail + 1;
+        return size;
     }
 
     private void resize(int newSize) {
         Item[] tmp = (Item[]) new Object[newSize];
-
         int h = head;
-        int count = size();
-
-        for (int i = 0; i < count; i++) {
+        for (int i = 0; i < size; i++) {
             tmp[i] = items[h];
             h = (h + 1) % items.length;
         }
 
         items = tmp;
         head = 0;
-        tail = count - 1;
+        tail = size - 1;
     }
 
     public void enqueue(Item item) {
-        if (size() == items.length) resize(items.length * 2);
+        if (size == items.length) resize(items.length * 2);
 
-        if (size() == 0) {
-            items[0] = item;
-            head = 0;
-            tail = 0;
-        } else {
-            items[++tail] = item;
-        }
+        if (head == -1) head = 0;
+
+        if (++tail == items.length) tail = 0;
+
+        items[tail] = item;
+        size++;
     }
 
     public Item dequeue() {
-        if (size() == 0)
+        if (size == 0)
             throw new NoSuchElementException("Queue is empty!");
 
-        Item item;
-        if (head == tail) {
-            item = items[head];
-            head = -1;
-            tail = -1;
-        } else {
-            item = items[head];
-            head++;
-        }
+        Item item = items[head++];
+
+        if (head == items.length) head = 0;
+
+        size--;
 
         return item;
     }
@@ -73,8 +65,7 @@ public class Queue<Item> implements Iterable<Item> {
 
         public QueueIterator() {
             h = head;
-            t = tail;
-            count = size();
+            count = size;
         }
 
         public boolean hasNext() {
@@ -110,12 +101,9 @@ public class Queue<Item> implements Iterable<Item> {
         q.enqueue(2);
         q.enqueue(3);
         q.enqueue(4);
+        q.dequeue();
+        q.dequeue();
         q.enqueue(5);
-        q.dequeue();
-        q.dequeue();
-        q.dequeue();
-        q.dequeue();
-        q.dequeue();
         q.enqueue(6);
         q.enqueue(7);
         q.dequeue();
